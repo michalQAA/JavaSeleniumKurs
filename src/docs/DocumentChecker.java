96ad3a0ab27e297b741a591340bb664cc7bb0d7a
MichalQA <michal.bylica@tsh.io>
2022-09-28 00:43:16 +0200

    commit polimorfizm

---
+++ src/docs/DocumentChecker.java
@@ -0,0 +1,9 @@
+package docs;
+
+public class DocumentChecker {
+    public static void main(String[] args) {
+        Document pdfdoc = new PdfDocument();
+        pdfdoc.getDescription();
+
+    }
+}
